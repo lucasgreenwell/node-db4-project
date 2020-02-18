cdd0b23b9fb2e311f7c4dde2aa9efa753cc18138
Lucas Greenwell <lucasgreenwell15@gmail.com>
2020-02-18 11:13:23 -0500

    database created

---
+++ migrations/20200218101246_create-recipes.js
@@ -0,0 +1,26 @@
+
+exports.up = function (knex) {
+    return knex.schema.createTable('recipes', tbl => {
+        tbl.increments();
+        tbl.text('recipeName', 128).notNullable()
+    }).createTable('ingredients', tbl => {
+        tbl.increments();
+        tbl.text('ingredientName', 128).notNullable()
+    }).createTable('recipesIngredients', tbl => {
+        tbl.integer('recipeId')
+            .unsigned()
+            .notNullable()
+            .references('id')
+            .inTable('recipes')
+        tbl.integer('ingredientId')
+            .unsigned()
+            .notNullable()
+            .references('id')
+            .inTable('ingredients')
+            tbl.text('quantity', 128)
+    })
+};
+
+exports.down = function (knex) {
+    return knex.schema.dropTableIfExists('recipesIngredients').dropTableIfExists('Ingredients').dropTableIfExists('recipes')
+};
